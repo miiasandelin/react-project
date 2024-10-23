4b596f4174bff05d4e966f7d2e4b3ceb3897a798
miiasandelin <n3sami00@students.oamk.fi>
2024-10-23 22:07:10 +0300

    API call, mapping

---
+++ src/components/fact.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const fact = () => {
+  return (
+	<div>fact</div>
+  )
+}
--- src/pages/RandomDogFacts.jsx
+++ src/pages/RandomDogFacts.jsx
@@ -1,7 +1,29 @@
+import axios from 'axios';
 import React from 'react'
+import { useEffect, useState } from 'react'
 
 export const RandomDogFacts = () => {
-  return (
-	<div>RandomDogFacts</div>
-  )
+
+	const [Facts, setFacts] = useState();
+	const APIURL = "https://dog-api.kinduff.com/api/facts?number=5"
+
+	useEffect(() => {
+		axios.get(APIURL).then((response) => {
+			setFacts(response.data["facts"])
+		});
+	}, [])
+
+
+
+	return (
+		<div>
+			{Facts?.map((fact) => {
+				return (
+					<div>
+						{fact}
+					</div>
+				)
+			})}
+		</div>
+	)
 }
