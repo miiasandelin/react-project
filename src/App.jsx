ee4dc81831d2a73961984647bd1a7720151fb82c
miiasandelin <n3sami00@students.oamk.fi>
2024-10-23 21:26:49 +0300

    Router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import { RandomDogFacts } from './pages/RandomDogFacts'
 
 function App() {
 
   return (
-    <>
-        
-    </>
+	<>
+    <Routes>
+		<Route path="/" element={<RandomDogFacts/>}></Route>
+    </Routes>
+	</>
   )
 }
 
---
+++ src/pages/RandomDogFacts.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const RandomDogFacts = () => {
+  return (
+	<div>RandomDogFacts</div>
+  )
+}
